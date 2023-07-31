65002528292a67cda8592df1b57950f7e9898032
Michael Lund <michael.lund@oslofjord.com>
2023-07-31 09:53:48 +0200

    added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,8 @@
 import React, { useState } from "react";
-// import Klikkit from "./projects/Klikkit";
-// import Digikort from "./projects/Digikort";
-// import OCC from "./projects/OCC";
 import ThreeTest from "./ThreeTest";
 
 export default function Home() {
   const [mainContent, setMainContent] = useState(true);
-  const [showInfo, setShowInfo] = useState({
-    show: false,
-    info: "",
-  });
   return (
     <>
       {mainContent ? (
@@ -20,6 +13,9 @@ export default function Home() {
             className="hero-image"
             id="backgroundImage"
             draggable={false}
+            style={{
+              zIndex: 0,
+            }}
           />
           <div className="hero fancyAnimateIn">
             <h1>Michael Lund</h1>
@@ -30,6 +26,44 @@ export default function Home() {
               I am ceaselessly driven by my passion for innovative and impactful
               solutions that transform and streamline the lives of individuals
               globally.
+            </p>
+            <p>
+              Welcome to my personal website. I'm an experienced and dedicated
+              software developer and architect, with a deep passion for
+              optimizing, scaling, and designing both intricate and robust
+              systems.
+            </p>
+            <p>
+              In my career, I have mastered a variety of programming languages
+              including .Net, Java, JavaScript, Python, and most notably, Go
+              (Golang). I have a special affinity for Go's simplicity,
+              efficiency, and its powerful support for concurrent programming,
+              which makes it my go-to choice for creating high-performing,
+              server-side applications and modern, cloud-native solutions.
+            </p>
+            <p>
+              As part of my ongoing commitment to stay on the cutting edge of
+              technology, I've recently embarked on an exciting journey to learn
+              Rust. Known for its performance and safety, Rust is an innovative
+              tool for system programming and creating software that's free from
+              common bugs related to memory safety. I'm eager to explore Rust's
+              potential to create highly efficient software with its minimal
+              runtime and precise control over system resources.
+            </p>
+            <p>
+              Throughout my career, I've also had the pleasure to create a
+              complete SaaS booking platform from the ground up. This platform
+              is a testament to my comprehensive understanding of software
+              development, ability to design scalable systems, and proficiency
+              in bringing complex projects to fruition.
+            </p>
+            <p>
+              As a seasoned professional and lifelong learner, I continually
+              strive to perfect my craft, explore new technologies, and create
+              sophisticated, efficient solutions that meet both technical
+              requirements and user needs. Feel free to explore my website to
+              learn more about my projects and my journey in software
+              development.
             </p>
           </div>
 
@@ -49,25 +83,13 @@ export default function Home() {
                 </p>
                 <p>
                   I am responsible for the development of the Klikkit system
+                  which is a SaaS booking platform for the hospitality industry.
                 </p>
-                <button
-                  type="button"
-                  onClick={() => {
-                    if (showInfo.show) {
-                      setShowInfo({
-                        show: false,
-                        info: "",
-                      });
-                    } else {
-                      setShowInfo({
-                        show: true,
-                        info: "Klikkit",
-                      });
-                    }
-                  }}
-                >
-                  Tell me more
-                </button>
+                <p>
+                  The system is seamlessly integrated with continously development
+                  of new features and improvements.
+                </p>
+               
               </div>
               <div className="project__card">
                 <h3>OCC</h3>
@@ -83,24 +105,7 @@ export default function Home() {
                   Payroll platform integrated with the OCC system for time
                   tracking and staff management.
                 </p>
-                <button
-                  type="button"
-                  onClick={() => {
-                    if (showInfo.show) {
-                      setShowInfo({
-                        show: false,
-                        info: "",
-                      });
-                    } else {
-                      setShowInfo({
-                        show: true,
-                        info: "OCC",
-                      });
-                    }
-                  }}
-                >
-                  Tell me more
-                </button>
+               
               </div>
               <div className="project__card">
                 <h3>Digikort.no</h3>
@@ -117,24 +122,26 @@ export default function Home() {
                   with the goal of making it easy for the customer to create and
                   send gift-cards to their friends and family online.
                 </p>
-                <button
-                  type="button"
-                  onClick={() => {
-                    if (showInfo.show) {
-                      setShowInfo({
-                        show: false,
-                        info: "",
-                      });
-                    } else {
-                      setShowInfo({
-                        show: true,
-                        info: "Digikort",
-                      });
-                    }
-                  }}
-                >
-                  Tell me more
-                </button>
+                
+              </div>
+              <div className="project__card">
+                <h3>Kvelde helse</h3>
+                <span>
+                  Tech: React, MongoDB, Go, Cloudflare,
+                  Twilio, Stripe, Visma eAccounting
+                </span>
+                <p>
+                  I am creating a membership platform for Kvelde Helse. The
+                  platform is built with the goal of making it easy for the
+                  customer to sign-up for a membership and pay for the
+                  membership online.
+                </p>
+                <p>
+                  I have created the platform to be easy to use and easy to
+                  maintain. The platform is built with a modern tech-stack and
+                  is hosted on a cloud platform.
+                </p>
+               
               </div>
             </div>
           </div>
